86dce2913f7b40d834b01b3e83e2822fb9bb35c6
shane.nguyen <shane.nguyen@starixsoft.com>
2021-04-20 13:53:29 +0700

    UPdate run on chrome

--- selenium/webdriver/Topic_02_Xpart_Css.java
+++ selenium/webdriver/Topic_02_Xpart_Css.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -12,10 +13,12 @@ import org.testng.annotations.Test;
 
 public class Topic_02_Xpart_Css {
 	WebDriver driver;
+	String projectpath = System.getProperty("user.dir");
 
 	@BeforeClass
 	public void beforeClass() {
-		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver",projectpath + "\\BrowserDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
